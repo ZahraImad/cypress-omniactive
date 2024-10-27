4d1a0cf89c3201541bb200b44bcee124f71b85ad
Zahra <zahra@Zahras-Macbook-Pro.local>
2024-10-27 19:49:35 +0200

    Adding cypress automation code

---
+++ cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,38 @@
+describe("MyFirstTest", () => {
+    // This will run before each test case
+    beforeEach(() => {
+      cy.visit('https://www.omniactivefitness.com/'); // Open the website
+    });
+  
+    it("Testcase001 - Verify Title page - Positive", () => {
+      // Verify that the page title contains "Muhammad Ali"
+      cy.title().should('contain', 'Personal Training');
+    });
+  
+    it("Testcase002 - Go to Contact us page", () => {
+      cy.get('#comp-krajyxkk7label').click();
+      cy.url().should('contain', "contact")
+      .and('exist')
+    });
+  
+    it("Testcase003 - Fill the contact us form", () => {
+      cy.get('#comp-krajyxkk7label').click()
+      //cy.title().should('contain',"Contact Us")
+
+      cy.get('#input_comp-lvr8f79e2').type('Zaa') //firstname
+      cy.get('#input_comp-lvr8f79h4').type('Im') //lastname
+      cy.get('#input_comp-m18nse4z').type('+3584529999000') //phonenumber
+      cy.get('#input_comp-lvr8f79j1').type('Zaa@z.com')  //email
+      cy.get('#textarea_comp-lvr8f79k6').type('Hello, Trying to know if this email works or not')
+      cy.get('#collection_comp-lvr8f79m6').click()
+      setTimeout(6000)
+      cy.get('._Gybpq').select('Fundraisers')
+      setTimeout(6000)
+      //cy.get('#collection_comp-lvr8f79m6').select('Fundraisers')
+      cy.get('[data-testid="buttonElement"]').click()
+      cy.get('.error-message').should('be.visible') 
+      .and('contain', 'successful')
+
+      
+    });
+  });
